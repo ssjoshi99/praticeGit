a1066a69a3108be220a793317ea558ec55388089
ssjoshi99 <joshisumit99@gmail.com>
2024-05-26 21:57:37 +0530

    conflict resolved

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 def add(a,b,c):
     return a+b+c
 
+<<<<<<< HEAD
 def sub(a,b,e):
     return a-b-e
+=======
+def sub(a,b,d):
+    return a-b-d
+>>>>>>> 43c6c99 (Changes)
 
 def div(a,b):
     return a/b
